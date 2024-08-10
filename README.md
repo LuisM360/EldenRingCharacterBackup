5048b3274b88816b6851470f7c494ddff0bfa6ee
Farmer GPSY <59676100+FarmerGPSY@users.noreply.github.com>
2024-08-10 09:01:17 -0400

    Updated README.md

--- README.md
+++ README.md
@@ -1,11 +1,26 @@
 # Elden Ring Character Save Backup Tool
-Backup and Restore your Elden Ring Character Save
+A simple tool to backup and restore your Elden Ring character save folder
 
-## Installation & Usage
-1. Download lastest Release
-2. Run Program
+## Features
+Backup: Easily create a backup of your entire Elden Ring save folder, including all files within it, to any location.
+Restore: Restore your save folder from a backup, ensuring all progress files are restored intact.
 
-### Usage
-Once the program runs and opens. There will be two buttons Backup and Restore. Backup will ask you to choose where you
-want to save the files to. Restore will ask which folder you want to restore from. It will then replace the file in the
-original save folder with the files from the folder chosen.
+
+
+
+## Installation & Running the Program
+1. Download the latest version of the tool from the Release page.
+2. Double-click on the executable file
+3. The program will open a GUI with two main options: Backup and Restore.
+
+
+## Usage
+Backup
+1. Click the Backup button.
+2. A dialog box will prompt you to select a destination folder where you want to save the backup.
+3. After selecting the destination, the program will create a backup of your entire Elden Ring save folder, including all files inside it, in the chosen folder.
+
+Restore
+1. Click the Restore button.
+2. A dialog box will prompt you to select the folder that contains your backup.
+3. The program will replace the current save folder, along with all files within it, in the original save directory with the backup folder from the selected location.
--- util/file_operations.py
+++ util/file_operations.py
@@ -2,7 +2,6 @@ import os
 import shutil
 from PySide6.QtWidgets import QFileDialog, QMessageBox
 
-
 class FileOperations:
     def __init__(self):
         self.backup_directory = ""
@@ -11,11 +10,11 @@ class FileOperations:
         source_path = os.path.expanduser("~\\AppData\\Roaming\\EldenRing")
         destination_path = QFileDialog.getExistingDirectory(None, "Select Backup Directory")
         if destination_path:
-            self.backup_directory = destination_path
+            self.backup_directory = self.create_backup_folder(destination_path)
             try:
                 for item in os.listdir(source_path):
                     src_item = os.path.join(source_path, item)
-                    dst_item = os.path.join(destination_path, item)
+                    dst_item = os.path.join(self.backup_directory, item)
                     if os.path.isdir(src_item):
                         shutil.copytree(src_item, dst_item, dirs_exist_ok=True)
                     else:
@@ -23,6 +22,14 @@ class FileOperations:
                 QMessageBox.information(None, "Success", "Backup completed successfully!")
             except Exception as e:
                 QMessageBox.critical(None, "Error", str(e))
+
+    def create_backup_folder(self, destination_path):
+        existing_backups = [d for d in os.listdir(destination_path) if d.startswith('Backup') and d[6:].isdigit()]
+        backup_numbers = [int(d[6:]) for d in existing_backups]
+        next_backup_number = max(backup_numbers, default=0) + 1
+        new_backup_folder = os.path.join(destination_path, f'Backup{next_backup_number}')
+        os.makedirs(new_backup_folder)
+        return new_backup_folder
 
     def restore(self, backup_directory):
         source_path = backup_directory
